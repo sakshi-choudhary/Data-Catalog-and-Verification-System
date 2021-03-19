cb0c26924d76194fb078332e854dd8612f5852a9
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-03-19 18:06:12 +0530

    🎉 init react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="overflow-hidden bg-gray-50">
+        <h1 className="text-2xl mx-2 mb-10 text-center tracking-tight font-extrabold text-gray-600 sm:text-5xl md:text-6xl">
+          DashBoard
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
